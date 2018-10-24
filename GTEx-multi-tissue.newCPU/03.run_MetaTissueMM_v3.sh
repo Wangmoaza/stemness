9b843d8b2e49c67f961297b12b44d0e15209f8be
Wangmoaza <haeun0512@hanmail.net>
2018-10-24 19:54:43 +0900

    SI-QTL qqplot

--- GTEx-multi-tissue.newCPU/03.run_MetaTissueMM_v3.sh
+++ GTEx-multi-tissue.newCPU/03.run_MetaTissueMM_v3.sh
@@ -13,18 +13,18 @@ parallel --env FILE_PATH --eta -j 40 --load 80% --xapply --noswap '/home/omics/h
 --full_metasoft_output \
 --savebeta \
 --start_snp_index {1} \
---end_snp_index {2}' ::: {400000..7980000..10000} ::: {410000..7990000..10000}
+--end_snp_index {2}' ::: {0..190000..10000} ::: {10000..200000..10000}
 
-/home/omics/haeun/program/MetaTissueMM \
---expr $FILE_PATH/02_MetaTissue_input/output_gene.txt \
---geno $FILE_PATH/02_MetaTissue_input/output_snp.txt \
---matrix $FILE_PATH/02_MetaTissue_input/matrix.txt \
---output $FILE_PATH/03_MetaTissue_output/GTEx_TOIL_white_MetaTissue \
---cov $FILE_PATH/02_MetaTissue_input/output_cov.txt \
---metatissue_bin_path /home/omics/haeun/program/Meta-Tissue.v.0.5/Metasoft \
---n_digits 5 \
---full_metasoft_output \
---savebeta \
---start_snp_index 7990000 \
---end_snp_index 7991910
+#/home/omics/haeun/program/MetaTissueMM \
+#--expr $FILE_PATH/02_MetaTissue_input/output_gene.txt \
+#--geno $FILE_PATH/02_MetaTissue_input/output_snp.txt \
+#--matrix $FILE_PATH/02_MetaTissue_input/matrix.txt \
+#--output $FILE_PATH/03_MetaTissue_output/GTEx_TOIL_white_MetaTissue \
+#--cov $FILE_PATH/02_MetaTissue_input/output_cov.txt \
+#--metatissue_bin_path /home/omics/haeun/program/Meta-Tissue.v.0.5/Metasoft \
+#--n_digits 5 \
+#--full_metasoft_output \
+#--savebeta \
+#--start_snp_index 7990000 \
+#--end_snp_index 7991910
 
--- GTEx-multi-tissue.newCPU/04.run_Metasoft.sh
+++ GTEx-multi-tissue.newCPU/04.run_Metasoft.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-parallel --eta -j 20 --load 80% --noswap '/home/omics/DATA_CPU/haeun/stemness/03_MetaTissue_output/GTEx_TOIL_white_MetaTissue.SNP.{}.metasoft.sh' ::: {2400000..6000000..10000}
+parallel --eta -j 20 --load 80% --noswap '/home/omics/DATA_CPU/haeun/stemness/03_MetaTissue_output/GTEx_TOIL_white_MetaTissue.SNP.{}.metasoft.sh' ::: {0..190000..10000}
